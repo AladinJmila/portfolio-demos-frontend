a4f229a2728ba5e1e3ebf3f669fa461d332f4fd9
AladinJmila <jmila.aladin@gmail.com>
2023-07-01 19:28:26 +0100

    Add studyroom data

--- src/data/db.ts
+++ src/data/db.ts
@@ -27,19 +27,67 @@ export const projectsList = [
     features: [
       {
         id: 11,
-        title: 'Feature 1',
-        gif: 'https://dummyimage.com/640x360/aaa/fff',
-        description: 'Description of Feature 1',
+        title: 'Overview',
+        gif: 'https://jmila-apps.s3.eu-west-1.amazonaws.com/static-assets/portfolio-demos/my-study-room/overview.gif',
+        description: '',
         codeSnippet:
-          'https://emgithub.com/embed-v2.js?target=https%3A%2F%2Fgithub.com%2FAladinJmila%2Fportfolio-demos%2Fblob%2Fmaster%2Fsrc%2Fcomponents%2FProjectCard.tsx&style=github-dark-dimmed&type=code&showLineNumbers=on&showFullPath=on',
+          'https://emgithub.com/embed-v2.js?target=https%3A%2F%2Fgithub.com%2FAladinJmila%2Fmy-studyroom-frontend%2Fblob%2Fmaster%2Fsrc%2FApp.js&style=github-dark-dimmed&type=code&showLineNumbers=on&showFullPath=on',
       },
       {
         id: 12,
-        title: 'Feature 2',
-        gif: 'https://dummyimage.com/640x360/aaa/fff',
-        description: 'Description of Feature 2',
+        title: 'Subjects',
+        gif: 'https://jmila-apps.s3.eu-west-1.amazonaws.com/static-assets/portfolio-demos/my-study-room/subjects.gif',
+        description: '',
         codeSnippet:
-          'https://emgithub.com/embed-v2.js?target=https%3A%2F%2Fgithub.com%2FAladinJmila%2Fportfolio-demos%2Fblob%2Fmaster%2Fsrc%2Fcomponents%2FProjectDetails.tsx&style=github-dark-dimmed&type=code&showLineNumbers=on&showFullPath=on',
+          'https://emgithub.com/embed-v2.js?target=https%3A%2F%2Fgithub.com%2FAladinJmila%2Fmy-studyroom-frontend%2Fblob%2Fmaster%2Fsrc%2Fsubjects-app%2Fcomponents%2FSubjects.jsx&style=github-dark-dimmed&type=code&showLineNumbers=on&showFullPath=on',
+      },
+      {
+        id: 13,
+        title: 'Sessions & Timer',
+        gif: 'https://jmila-apps.s3.eu-west-1.amazonaws.com/static-assets/portfolio-demos/my-study-room/sessions-and-timer.gif',
+        description: '',
+        codeSnippet:
+          'https://emgithub.com/embed-v2.js?target=https%3A%2F%2Fgithub.com%2FAladinJmila%2Fmy-studyroom-frontend%2Fblob%2Fmaster%2Fsrc%2Fschedules-app%2Fcomponents%2FSessions.jsx&style=github-dark-dimmed&type=code&showLineNumbers=on&showFullPath=on',
+      },
+      {
+        id: 14,
+        title: 'Notes',
+        gif: 'https://jmila-apps.s3.eu-west-1.amazonaws.com/static-assets/portfolio-demos/my-study-room/study-notes.gif',
+        description: '',
+        codeSnippet:
+          'https://emgithub.com/embed-v2.js?target=https%3A%2F%2Fgithub.com%2FAladinJmila%2Fmy-studyroom-frontend%2Fblob%2Fmaster%2Fsrc%2Fnotes-app%2FNotes.jsx&style=github-dark-dimmed&type=code&showLineNumbers=on&showFullPath=on',
+      },
+      {
+        id: 15,
+        title: 'Tasks',
+        gif: 'https://jmila-apps.s3.eu-west-1.amazonaws.com/static-assets/portfolio-demos/my-study-room/tasks.gif',
+        description: '',
+        codeSnippet:
+          'https://emgithub.com/embed-v2.js?target=https%3A%2F%2Fgithub.com%2FAladinJmila%2Fmy-studyroom-frontend%2Fblob%2Fmaster%2Fsrc%2Ftasks-app%2Fcomponents%2FTasks.jsx&style=github-dark-dimmed&type=code&showLineNumbers=on&showFullPath=on',
+      },
+      {
+        id: 16,
+        title: 'Resources',
+        gif: 'https://jmila-apps.s3.eu-west-1.amazonaws.com/static-assets/portfolio-demos/my-study-room/resources.gif',
+        description: '',
+        codeSnippet:
+          'https://emgithub.com/embed-v2.js?target=https%3A%2F%2Fgithub.com%2FAladinJmila%2Fmy-studyroom-frontend%2Fblob%2Fmaster%2Fsrc%2Fresources-app%2FResources.jsx&style=github-dark-dimmed&type=code&showLineNumbers=on&showFullPath=on',
+      },
+      {
+        id: 17,
+        title: 'Audio Notes',
+        gif: 'https://jmila-apps.s3.eu-west-1.amazonaws.com/static-assets/portfolio-demos/my-study-room/audio-notes.gif',
+        description: '',
+        codeSnippet:
+          'https://emgithub.com/embed-v2.js?target=https%3A%2F%2Fgithub.com%2FAladinJmila%2Fmy-studyroom-frontend%2Fblob%2Fmaster%2Fsrc%2Faudio-notes-app%2FAudioNotes.jsx&style=github-dark-dimmed&type=code&showLineNumbers=on&showFullPath=on',
+      },
+      {
+        id: 18,
+        title: 'Profile',
+        gif: 'https://jmila-apps.s3.eu-west-1.amazonaws.com/static-assets/portfolio-demos/my-study-room/profile.gif',
+        description: '',
+        codeSnippet:
+          'https://emgithub.com/embed-v2.js?target=https%3A%2F%2Fgithub.com%2FAladinJmila%2Fmy-studyroom-frontend%2Fblob%2Fmaster%2Fsrc%2Fcomponents%2FProfile.jsx&style=github-dark-dimmed&type=code&showLineNumbers=on&showFullPath=on',
       },
     ],
   },
@@ -84,7 +132,7 @@ export const projectsList = [
     id: 3,
     title: 'Dynamic Tutorials',
     type: 'Full Stack',
-    createdAt: 'MAR 2022',
+    createdAt: 'APR 2022',
     image:
       'https://jmila-apps.s3.eu-west-1.amazonaws.com/static-assets/portfolio-demos/dynamic-tutorials/cover.PNG',
     url: 'https://dynamic-tutorials-server.onrender.com/tutorials/show',
